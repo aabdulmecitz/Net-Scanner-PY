777ef2ea6382c371318f29031cf2d58c7b71e7ad
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-10-06 03:20:15 +0300

    sec commit

--- net_scanner.py
+++ net_scanner.py
@@ -1,6 +1,19 @@
 import scapy.all as scapy
 
-arp_package = scapy.ARP()
+# ARP request
+# broadcast
+# response
 
-scapy.ls(scapy.ARP)
+arp_package = scapy.ARP(pdst="192.168.0.1/24")
 
+#scapy.ls(scapy.ARP)
+
+broadcast_package = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+#scapy.ls(scapy.Ether)
+
+combined_pack = broadcast_package/arp_package
+
+(answered_list, unanswered_list) = scapy.srp(combined_pack, timeout=1)
+
+print(list(answered_list))
